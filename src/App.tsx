49e1f9661484853ff4a590fca28ea3a884a48383
Stanislav <orlandonetworks@gmail.com>
2023-04-15 22:09:06 +0300

    TaskCards dynamically updating

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import CardBlock from './components/card-block/card-block';
-import TaskCardWrapper from './components/task-card/task-card';
 import * as storage from './data_engine/localstorage';
 
 /*
@@ -32,18 +31,10 @@ function App() {
     <div className="App">
       <Header />
       <main className = 'card-wrapper'>
-        <CardBlock cardname = {FIRST_COLOUMN} >
-          <TaskCardWrapper cards = {storage.prepareCards(FIRST_COLOUMN.toLowerCase())} />
-        </CardBlock>
-        <CardBlock cardname = {SECOND_COLOUMN}>
-          <TaskCardWrapper cards = {storage.prepareCards(SECOND_COLOUMN.toLowerCase())} />
-        </CardBlock>
-        <CardBlock cardname = {THIRD_COLOUMN}>
-          <TaskCardWrapper cards = {storage.prepareCards(THIRD_COLOUMN.toLowerCase())} />
-        </CardBlock>
-        <CardBlock cardname = {FOURTH_COLOUMN}>
-          <TaskCardWrapper cards = {storage.prepareCards(FOURTH_COLOUMN.toLowerCase())} />
-        </CardBlock>
+        <CardBlock cardname = {FIRST_COLOUMN} />
+        <CardBlock cardname = {SECOND_COLOUMN} />
+        <CardBlock cardname = {THIRD_COLOUMN} />
+        <CardBlock cardname = {FOURTH_COLOUMN} />
       </main>
       <Footer active={1} finished={1} author = '<NAME>' year={2023} />
     </div>
--- src/components/card-block/card-block.tsx
+++ src/components/card-block/card-block.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './card-block.css';
 import * as storage from '../../data_engine/localstorage';
+import TaskCardWrapper from '../task-card/task-card';
 
 interface CardBlockProps {
     cardname: string
-    children: React.ReactNode
     cardMakerState?:  boolean
 }
 
@@ -14,15 +14,8 @@ const PREPARE_BUTTON_TEXT = '+ Add card';
 const CardBlock: React.FC<CardBlockProps> = (props:CardBlockProps) => {
     const [modeSwitch, changeMode] = useState(false);
 
-    let PrepareButton: JSX.Element
+    let PrepareButton: JSX.Element;
     let buttonRole = props.cardname.toLocaleLowerCase();
-    let displayState = 'none';
-    let postButtonClass = '';
-
-    if (modeSwitch){
-        postButtonClass = 'post-button';
-        displayState = 'block';
-    }
 
     PrepareButton = 
         <button 
@@ -34,7 +27,7 @@ const CardBlock: React.FC<CardBlockProps> = (props:CardBlockProps) => {
                     changeMode(!modeSwitch); 
                 }
             }
-            className = {`canvas-button ${postButtonClass}`}
+            className = {`canvas-button ${modeSwitch ? 'post-button' : ''}`}
         >
             {modeSwitch ? POST_BUTTON_TEXT : PREPARE_BUTTON_TEXT}
         </button>
@@ -43,9 +36,9 @@ const CardBlock: React.FC<CardBlockProps> = (props:CardBlockProps) => {
         <div className='card-block'>
             {props.cardname}
             <div className = 'card-canvas'>
-                {props.children}
+                <TaskCardWrapper cards = {storage.prepareCards(props.cardname.toLowerCase())} />
             </div>
-            <input className = 'card-name-input task-card' placeholder='Enter task name' style={{display: displayState}} />
+            <input className = 'card-name-input task-card' placeholder='Enter task name' style={{display: modeSwitch ? 'block' : 'none'}} />
             {PrepareButton}
         </div>
     )
